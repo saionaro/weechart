bcfeff7af119819e266b77f36189fff719a58aa0
Saionaro <shibakow@gmail.com>
2019-09-21 23:39:57 +0300

    Add test stand

---
+++ stand/src/index.js
@@ -0,0 +1,41 @@
+import { Chart } from "weechart";
+import "weechart/dist/bundle.css";
+
+let appElement;
+
+function onFetchData(data) {
+  const fragment = document.createDocumentFragment();
+  const h = 350;
+  let w = document.documentElement.clientWidth * 0.8;
+
+  w = w > 400 ? 400 : w;
+
+  for (let i = 0; i < data.length; i++) {
+    const chartContainer = document.createElement("div");
+    chartContainer.className = "chart";
+    new Chart(chartContainer, data[i], { w, h });
+    fragment.appendChild(chartContainer);
+  }
+
+  appElement.querySelector(".app__charts").appendChild(fragment);
+}
+
+function fetchData(url) {
+  return fetch(url)
+    .then(data => data.json())
+    .catch(console.log);
+}
+
+function bootUp() {
+  const urls = [];
+
+  appElement = document.querySelector(".app");
+
+  for (let i = 1; i <= 5; i++) {
+    urls.push(`./data/${i}/overview.json`);
+  }
+
+  Promise.all(urls.map(fetchData)).then(onFetchData);
+}
+
+document.addEventListener("DOMContentLoaded", bootUp);
